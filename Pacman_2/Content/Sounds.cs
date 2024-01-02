f1acacd1a95afc16c635e49b1473e8295749874f
TariqNier <154991066+TariqNier@users.noreply.github.com>
2024-01-02 19:13:28 +0200

    Add files via upload

---
+++ Pacman_2/Content/Sounds.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman_2
+{
+    public static class Sounds
+    {
+        public static SoundEffect game_start;
+        public static SoundEffect munch;
+        public static SoundEffectInstance munchInstance;
+        public static SoundEffect credit;
+        public static SoundEffect death_1;
+        public static SoundEffect death_2;
+        public static SoundEffect eat_fruit;
+        public static SoundEffect eat_ghost;
+        public static SoundEffect power_pellet;
+        public static SoundEffectInstance power_pellet_instance;
+        public static SoundEffect extend;
+        public static SoundEffect intermission;
+        public static SoundEffect retreating;
+        public static SoundEffectInstance retreatingInstance;
+        public static SoundEffect siren_1;
+        public static SoundEffectInstance siren_1_instance;
+        public static SoundEffect siren_2;
+        public static SoundEffect siren_3;
+        public static SoundEffect siren_4;
+        public static SoundEffect siren_5;
+    }
+}
